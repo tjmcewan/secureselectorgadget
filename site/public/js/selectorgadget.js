4416bfd3acc1bb224961526fe2df0da133bb561e
Tim McEwan <tim@mcewan.it>
2013-04-27 14:05:59 +1000

    first attempt

---
+++ site/public/js/selectorgadget.js
@@ -0,0 +1,51 @@
+// Copyright (c) 2008, 2009 Andrew Cantino
+// Copyright (c) 2008, 2009 Kyle Maxwell
+
+function importJS(src, look_for, onload) {
+  var s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', src);
+  if (onload) wait_for_script_load(look_for, onload);
+  var head = document.getElementsByTagName('head')[0];
+  if (head) {
+    head.appendChild(s);
+  } else {
+    document.body.appendChild(s);
+  }
+}
+
+function importCSS(href, look_for, onload) {
+  var s = document.createElement('link');
+  s.setAttribute('rel', 'stylesheet');
+  s.setAttribute('type', 'text/css');
+  s.setAttribute('media', 'screen');
+  s.setAttribute('href', href);
+  if (onload) wait_for_script_load(look_for, onload);
+  var head = document.getElementsByTagName('head')[0];
+  if (head) {
+    head.appendChild(s);
+  } else {
+    document.body.appendChild(s);
+  }
+}
+
+function wait_for_script_load(look_for, callback) {
+  var interval = setInterval(function() {
+    if (eval("typeof " + look_for) != 'undefined') {
+      clearInterval(interval);
+      callback();
+    }
+  }, 50);
+}
+
+(function(){
+  importCSS('https://selectorgadget.herokuapp.com/css/selectorgadget.css');
+  importJS('https://selectorgadget.herokuapp.com/vendor/jquery.min.js', 'jQuery', function() { // Load everything else when it is done.
+    jQuery.noConflict();
+    importJS('https://selectorgadget.herokuapp.com/vendor/diff_match_patch.js', 'diff_match_patch', function() {
+      importJS('https://selectorgadget.herokuapp.com/js/dom.js', 'DomPredictionHelper', function() {
+        importJS('https://selectorgadget.herokuapp.com/js/interface.js');
+      });
+    });
+  });
+})();
